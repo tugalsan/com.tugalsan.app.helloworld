11f6a656079141ef65fad51a97d73dc07809934f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-01-16 10:25:25 +0300

    Update AppStrings.java

--- com.tugalsan.app.helloworld-client/src/main/java/com/tugalsan/app/helloworld/AppStrings.java
+++ com.tugalsan.app.helloworld-client/src/main/java/com/tugalsan/app/helloworld/AppStrings.java
@@ -6,7 +6,7 @@ import com.tugalsan.api.charset.client.TGS_CharSetLocaleTypes;
 public class AppStrings {
 
     public static String TITLE_AppModuleDefault() {
-        return TGS_CharSetCast.current().localType.equals(TGS_CharSetLocaleTypes.TURKISH) ? "HelloWorld" : "HelloWorld";
+        return TGS_CharSetCast.current().localType.equals(TGS_CharSetLocaleTypes.TURKISH) ? "Paint" : "Resim";
     }
     public static String TITLE_AppModuleEcho() {
         return TGS_CharSetCast.current().localType.equals(TGS_CharSetLocaleTypes.TURKISH) ? "Eko" : "Echo";
