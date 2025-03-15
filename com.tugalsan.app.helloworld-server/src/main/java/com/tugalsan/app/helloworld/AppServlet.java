ec9e2d1076664c75119226e26733c544a5478c02
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-03-15 18:44:36 +0300

    Update AppServlet.java

--- com.tugalsan.app.helloworld-server/src/main/java/com/tugalsan/app/helloworld/AppServlet.java
+++ com.tugalsan.app.helloworld-server/src/main/java/com/tugalsan/app/helloworld/AppServlet.java
@@ -2,14 +2,7 @@ package com.tugalsan.app.helloworld;
 
 import com.tugalsan.api.file.common.client.TGS_FileCommonFavIcon;
 import com.tugalsan.api.log.server.TS_Log;
-import com.tugalsan.api.log.server.TS_LogUtils;
 import com.tugalsan.api.servlet.gwt.server.*;
-import com.tugalsan.api.servlet.url.server.TS_SURLWebServlet;
-import com.tugalsan.api.string.client.TGS_StringUtils;
-import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
-import com.tugalsan.api.tomcat.server.TS_TomcatPathUtils;
-import com.tugalsan.app.helloworld.AppSGEEcho;
-import com.tugalsan.lib.boot.server.TS_LibBootPathUtils;
 import com.tugalsan.lib.boot.server.TS_LibBootUtils;
 import java.time.Duration;
 import javax.servlet.ServletContextEvent;
@@ -26,22 +19,10 @@ public class AppServlet implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent evt) {
-        APP_NAME = TS_TomcatPathUtils.getWarNameLabel(evt);
-        TS_LogUtils.MAP = txt -> TGS_StringUtils.cmn().concat("[", APP_NAME, "] ", txt);
-        TS_SGWTWebServlet.killTrigger = TS_SURLWebServlet.killTrigger = TS_ThreadSyncTrigger.of(APP_NAME);
-        var contextKillTrigger = TS_SURLWebServlet.killTrigger.newChild("contextInitialized");
-        var appName = TS_TomcatPathUtils.getWarNameLabel(evt);
-        var pathServletConfig = TS_LibBootPathUtils.dirDat().value().resolve("cfg").resolve(appName);
-        var u_sConfig = TS_SGWTConfig.of(pathServletConfig, appName);
-        if (u_sConfig.isExcuse()) {
-            d.ce("contextInitialized", u_sConfig.excuse().getMessage());
-            return;
-        }
-        TS_SGWTWebServlet.config = u_sConfig.value();
-        TS_LibBootUtils.contextInitializedAsyncRun(contextKillTrigger,
+        APP_NAME = TS_LibBootUtils.warmUp_setLogPrefix_createKillTriggers_returnAppName_forApp(evt);
+        TS_LibBootUtils.contextInitializedAsyncRun(
                 Duration.ofMinutes(10),
-                evt, "autosqlweb", "ASW",
-                favIconSpi, favIconBug,
+                evt, "autosqlweb", "ASW", favIconSpi, favIconBug,
                 killTrigger -> {
                     TS_SGWTExecutorList.add(new AppSGEEcho()
                     );
